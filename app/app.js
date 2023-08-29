ddcde80c6d3330bffd23251cb07f0d49d1d7888c
Iz P <pehisaac@gmail.com>
2023-08-29 11:11:46 +0000

    early stages

---
+++ app/app.js
@@ -0,0 +1,155 @@
+const utilsJs = require('./utils.js')
+const discordInteractions = require('discord-interactions')
+const express = require('express')
+const awsServerlessExpressMiddleware = require('aws-serverless-express/middleware')
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+const axios = require('axios');
+
+const { Configuration, OpenAIApi } = require("openai");
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+
+const app = express()
+app.use(express.json({ verify: utilsJs.VerifyDiscordRequest(process.env.PUBLIC_KEY) }));
+app.use(awsServerlessExpressMiddleware.eventContext())
+
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "*")
+    next()
+});
+
+
+app.get('/health', function (req, res) {
+    res.json({ success: 'healthy!', url: req.url });
+});
+
+app.post('/discordgpt/interactions', async function (req, res) {
+    console.log('interactionsHandler');
+    // Interaction type and data
+    const { type, id, data, token, application_id } = req.body;
+    console.log(JSON.stringify(req.body));
+    console.log(req.body.application_id);
+    console.log(req.body.token);
+    /**
+     * Handle verification requests
+     */
+    if (type === discordInteractions.InteractionType.PING) {
+        return res.send({ type: discordInteractions.InteractionResponseType.PONG });
+    }
+
+    /**
+     * Handle slash command requests
+     * See https://discord.com/developers/docs/interactions/application-commands#slash-commands
+     */
+    if (type === discordInteractions.InteractionType.APPLICATION_COMMAND) {
+        const { name, options } = data;
+        console.log(options);
+        console.log(options[0].value);
+        const discord_webhook_id = application_id;
+        const discord_webhook_token = token;
+        const discord_webhook_message_id = req.body.channel.last_message_id;
+
+        // "gpt" command
+        if (name === 'gpt') {
+
+            try {
+                // quick response
+                let quickPayload = {
+                    type: discordInteractions.InteractionResponseType.DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE
+                };
+                // axios post request
+                let quickUrl = `https://discord.com/api/v10/interactions/${id}/${token}/callback`
+                const quickAxiosOptions = {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json; charset=UTF-8',
+                        Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
+                        'User-Agent': 'DiscordBot (https://github.com/discord/discord-example-app, 1.0.0)',
+                    },
+                    data: JSON.stringify(quickPayload),
+                    url: quickUrl,
+                };
+                axios(quickAxiosOptions)
+                    .then(function (response) {
+                        console.log(`quick discordRes: ${response}`);
+                    })
+                    .catch(function (error) {
+                        console.log(`quick err: ${error}`);
+                        if (error.response) {
+                            console.log(error.response.status);
+                            console.log(error.response.data);
+                        } else {
+                            console.log(error.message);
+                        }
+                    });
+
+
+
+
+
+
+
+
+
+
+                const response = await openai.createChatCompletion({
+                    model: "gpt-3.5-turbo",
+                    messages: [
+                        { "role": "system", "content": 'You are a helpful discord bot' },
+                        { "role": "user", "content": options[0].value }
+                    ]
+                })
+
+                console.log(`openai.createChatCompletion: ${response}`);
+                console.log('before');
+                // axios post request
+                let url = `https://discord.com/api/v10/webhooks/${application_id}/${discord_webhook_token}`
+                const axiosOptions = {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json; charset=UTF-8',
+                        Authorization: `Bot ${process.env.DISCORD_TOKEN}`,
+                        'User-Agent': 'DiscordBot (https://github.com/discord/discord-example-app, 1.0.0)',
+                    },
+                    data: JSON.stringify({
+                        content: `Question: ${options[0].value}
+Response:
+${response.data.choices[0].message.content}`,
+                    }),
+                    url,
+                };
+                const gptResponse = await axios(axiosOptions)
+                console.log(`gptResponse: ${gptResponse}`);
+                return res.send(200);
+
+                console.log('after');
+
+            } catch (error) {
+                if (error.response) {
+                    console.log(error.response.status);
+                    console.log(error.response.data);
+                } else {
+                    console.log(error.message);
+                }
+                return res.send({
+                    type: discordInteractions.InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
+                    data: {
+                        content: 'error running command ',
+                    },
+                });
+            }
+        }
+    }
+});
+
+app.listen(3000, function () {
+    console.log("App started")
+});
+
+
+module.exports = app
